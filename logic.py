b36e238648466646511959c6b267772c23b43382
Anton Ushakov <antonselkov63@gmail.com>
2025-05-01 17:50:59 +0500

    Изменение

--- logic.py
+++ logic.py
@@ -11,6 +11,9 @@ class Pokemon:
         self.pokemon_number = randint(1,1000)
         self.img = self.get_img()
         self.name = self.get_name()
+        self.power = randint(1, 5)
+        self.hp = randint(10, 20)
+        self.song = self.get_song()
 
         Pokemon.pokemons[pokemon_trainer] = self
 
@@ -24,6 +27,19 @@ class Pokemon:
         else:
             return "https://static.wikia.nocookie.net/anime-characters-fight/images/7/77/Pikachu.png/revision/latest/scale-to-width-down/700?cb=20181021155144&path-prefix=ru"
     
+    #Метод для получения звука покемона через API
+    def get_song(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            data = response.json() 
+            
+            return data["cries"]["latest"]
+        else:
+            return "Звук отсутствует у покемона"
+        
     # Метод для получения имени покемона через API
     def get_name(self):
         url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
@@ -37,11 +53,27 @@ class Pokemon:
 
     # Метод класса для получения информации
     def info(self):
-        return f"Имя твоего покеомона: {self.name}"
+        return {
+            "Фото": self.img,
+            "Название": self.name,
+            "Сила": self.power,
+            "Здоровье": self.hp,
+            "Звук": self.song
+        }
 
     # Метод класса для получения картинки покемона
     def show_img(self):
         return self.img
-
-
-
+    
+    def attack(self, enemy):
+        if self.hp >= 0:
+            enemy.hp -= self.power
+            if enemy.hp > 0:
+                return f"Вы нанесли урон. Здоровье врага: {enemy.hp}"
+            else:
+                return "Вы победили!"
+        else:
+            return "Ваш покемон мертв"
+        
+    
+        
--- main.py
+++ main.py
@@ -5,15 +5,55 @@ from logic import Pokemon
 
 bot = telebot.TeleBot(token) 
 
+
 @bot.message_handler(commands=['go'])
 def go(message):
     if message.from_user.username not in Pokemon.pokemons.keys():
         pokemon = Pokemon(message.from_user.username)
         bot.send_message(message.chat.id, pokemon.info())
         bot.send_photo(message.chat.id, pokemon.show_img())
+    
     else:
         bot.reply_to(message, "Ты уже создал себе покемона")
 
 
-bot.infinity_polling(none_stop=True)
+@bot.message_handler(commands=['attack'])
+def attack(message):
+    enemy_username = message.reply_to_message.from_user.username
+    my_username = message.from_user.username
 
+    if enemy_username not in Pokemon.pokemons:
+        bot.reply_to(message, "У этого человека нет покемона. Создать его можно с помощью /go")
+    
+    elif my_username not in Pokemon.pokemons:
+        bot.reply_to(message, "У вас нет покемона. Создайте его с помощью /go")
+    
+    else:
+        my_pokemon = Pokemon.pokemons[my_username]
+        enemy_pokemon = Pokemon.pokemons[enemy_username]
+        
+        result = my_pokemon.attack(enemy_pokemon)
+
+        bot.reply_to(message, result)
+
+
+@bot.message_handler(commands=['info'])
+def info(message):
+    user = message.from_user.username
+
+    if user in Pokemon.pokemons:
+        pokemon = Pokemon.pokemons[user]
+
+        text = Pokemon.info(pokemon)
+        result = ""
+
+        for value in text:
+            result += f"{value}: {text[value]}\n"
+
+        bot.send_message(message.chat.id, result)
+    else:
+        bot.send_message(message.chat.id, "Создайте покемона, с помощью /go")
+        
+
+if __name__ == "__main__":
+    bot.infinity_polling(none_stop=True)
